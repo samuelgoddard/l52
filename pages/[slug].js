b2d0548cd8557efc9a2d9c4ea44d92e5ff7a6823
Sam Goddard <hello@samgoddard.co.uk>
2021-07-05 15:55:21 +0100

    safari bug fixed

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,4 +1,3 @@
-
 import Layout from '@/components/layout'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
@@ -7,6 +6,8 @@ import { fade } from "@/helpers/transitions"
 import { LazyMotion, domAnimation, m } from 'framer-motion'
 import { NextSeo } from 'next-seo'
 import SanityPageService from '@/services/sanityPageService'
+import Div100vh from 'react-div-100vh'
+import ConditionalWrap from 'conditional-wrap';
 
 const query = `*[_type == "client" && slug.current == $slug][0]{
 	title,
@@ -35,59 +36,68 @@ export default function ClientSlug(initialData) {
     <Layout>
       <NextSeo title={title} />
       
-      <LazyMotion features={domAnimation}>      
-        <m.div
-          initial="initial"
-          animate="enter"
-          exit="exit"
-          className={`${collections?.length > 1 ? '' : 'flex flex-col h-screen' }`}
-        >
-          
-          <Header />
-
-          <m.div variants={fade} className="px-[25px] md:px-[35px] 2xl:px-[65px] w-full">
-            <h1 className="uppercase text-[28px] md:text-[32px] leading-none">{title}</h1>
-          </m.div>
-
-
-          {collections?.length > 0 ? (
-            <>
-              {collections.map((item, i) => {
-                return (
-                  <div key={i}>
-                    <m.div variants={fade} div className="w-full flex flex-wrap items-start md:items-center mb-5 px-[25px] md:px-[35px] 2xl:px-[65px]">
-                      <div className="w-auto">
-                        <span className="block uppercase text-xs md:text-sm text-blue">{item.title}</span>
-                      </div>
-                      <div className="ml-auto hidden md:block">
-                        <div className="md:flex md:items-center text-right">
-                          <span className="block uppercase text-xs md:text-sm md:ml-6">Enquire</span>
-                          <span className="block uppercase text-xs md:text-sm md:ml-6">Download Images</span>
-                          <span className="block uppercase text-xs md:text-sm md:ml-6">Download Lookbook</span>
-                        </div>
-                      </div>
-                    </m.div>
-        
-                    <m.div variants={fade} className={`w-full ${collections.length > 0 ? 'h-[70vh]' : '' }`}>
-                      <div className="w-full h-full flex items-center">
-                        <div className="w-full h-full">
-                          <Carousel slides={item.collectionImages} />
-                        </div>
-                      </div>
-                    </m.div>
-                  </div> 
-                )
-              })}
-            </>
-          ) : (
-            <m.span variants={fade} className="block px-[25px] md:px-[35px] 2xl:px-[65px] text-sm opacity-75">Collection Coming Soon</m.span>
+      <LazyMotion features={domAnimation}>
+        <ConditionalWrap
+          condition={!!collections?.length > 1}
+          wrap={children => (
+            <Div100vh>
+              {children}
+            </Div100vh>
           )}
-          
+        >  
+          <m.div
+            initial="initial"
+            animate="enter"
+            exit="exit"
+            className={`${collections?.length > 1 ? '' : 'flex flex-col h-full' }`}
+          >
+            
+            <Header />
 
-          <m.div variants={fade} className="w-full">
-            <Footer />
+            <m.div variants={fade} className="px-[25px] md:px-[35px] 2xl:px-[65px] w-full">
+              <h1 className="uppercase text-[28px] md:text-[32px] leading-none">{title}</h1>
+            </m.div>
+
+
+            {collections?.length > 0 ? (
+              <>
+                {collections.map((item, i) => {
+                  return (
+                    <div key={i}>
+                      <m.div variants={fade} div className="w-full flex flex-wrap items-start md:items-center mb-5 px-[25px] md:px-[35px] 2xl:px-[65px]">
+                        <div className="w-auto">
+                          <span className="block uppercase text-xs md:text-sm text-blue">{item.title}</span>
+                        </div>
+                        <div className="ml-auto hidden md:block">
+                          <div className="md:flex md:items-center text-right">
+                            <span className="block uppercase text-xs md:text-sm md:ml-6">Enquire</span>
+                            <span className="block uppercase text-xs md:text-sm md:ml-6">Download Images</span>
+                            <span className="block uppercase text-xs md:text-sm md:ml-6">Download Lookbook</span>
+                          </div>
+                        </div>
+                      </m.div>
+          
+                      <m.div variants={fade} className={`w-full ${collections.length > 0 ? 'h-[70vh]' : '' }`}>
+                        <div className="w-full h-full flex items-center">
+                          <div className="w-full h-full">
+                            <Carousel slides={item.collectionImages} />
+                          </div>
+                        </div>
+                      </m.div>
+                    </div> 
+                  )
+                })}
+              </>
+            ) : (
+              <m.span variants={fade} className="block px-[25px] md:px-[35px] 2xl:px-[65px] text-sm opacity-75">Collection Coming Soon</m.span>
+            )}
+            
+
+            <m.div variants={fade} className="w-full">
+              <Footer />
+            </m.div>
           </m.div>
-        </m.div>
+        </ConditionalWrap>
       </LazyMotion>
     </Layout>
   )
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import { fade } from "@/helpers/transitions"
 import { LazyMotion, domAnimation, m } from 'framer-motion'
 import SanityPageService from '@/services/sanityPageService'
 import { NextSeo } from 'next-seo'
+import Div100vh from 'react-div-100vh'
 
 const query = `{
   "clients": *[_type == "client"]{
@@ -32,27 +33,29 @@ export default function Home(initialData) {
       <NextSeo title="Home" />
       
       <LazyMotion features={domAnimation}>      
-        <m.div
-          initial="initial"
-          animate="enter"
-          exit="exit"
-          className="flex flex-col h-screen"
-        >
-          
-          <Header indexActive />
+        <Div100vh>
+          <m.div
+            initial="initial"
+            animate="enter"
+            exit="exit"
+            className="flex flex-col h-full"
+          >
+            
+            <Header indexActive />
 
-          <m.div variants={fade} className="w-full flex flex-grow">
-            <div className="w-full flex flex-grow justify-center items-center">
-              <div className="w-full h-full">
-                <Carousel slides={clients} teaser />
+            <m.div variants={fade} className="w-full flex flex-grow">
+              <div className="w-full flex flex-grow justify-center items-center">
+                <div className="w-full h-full">
+                  <Carousel slides={clients} teaser />
+                </div>
               </div>
-            </div>
-          </m.div>
+            </m.div>
 
-          <m.div variants={fade} className="w-full">
-            <Footer />
+            <m.div variants={fade} className="w-full">
+              <Footer />
+            </m.div>
           </m.div>
-        </m.div>
+        </Div100vh>
       </LazyMotion>
     </Layout>
   )
