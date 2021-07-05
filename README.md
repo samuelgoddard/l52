945a393a50ab4b065b2b504442eda60e128fec6f
Sam Goddard <hello@samgoddard.co.uk>
2021-07-05 10:55:43 +0100

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Next Tailwind Framer Motion Boilerplate
+A simple [Next.js](https://nextjs.org/) starter kit.
+
+**Features:**
+- Next 11
+- Tailwind (With JIT mode enabled)
+- Custom PostCSS setup to allow for `@import` and simple CSS nesting
+- Framer Motion 4 (With LazyMotion setup for smaller bundle sizes)
+- Preact configured instead of React for smaller bundle size*
+- SEO preconfigured with [next-seo](https://github.com/garmeeh/next-seo)
+- jsconfig component/helper/styles/public maps (allows you to use `@/components/...` instead of relative paths)
+
+_*To remove Preact and switch back to React simply remove the code in `next.config.js`_
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false,
+  theme: {
+    fontFamily: {
+      'sans': ['Raglan Sans', 'Arial', 'sans-serif'],
+      'mono': ['Space Mono', 'Arial', 'sans-serif'],
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1600px",
+      "3xl": "1920px"
+    },
+    extend: {
+      colors: {
+        'black': '#000',
+        'white': '#FFF',
+        color: {
+          light: '#ffb288',
+          default: '#d18d67',
+          dark: '#ce8860',
+        },
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: []
+}
